21dcb7301632d6c1fa023444db941d6e2e8bef5d
Daniel Alves <dnl.alv@gmail.com>
2020-01-11 17:39:08 -0300

    removido indentity feita autenticação de forma simples

---
+++ src/Knewin.CityApi/Controllers/AccountController.cs
@@ -0,0 +1,69 @@
+﻿using AutoMapper;
+using Knewin.CityApi.ViewModels.Account;
+using Knewin.Domain.Entities;
+using Knewin.Infra.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Knewin.Core.Extensions;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace Knewin.CityApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]"), ApiVersion("1")]
+    public class AccountController : Controller
+    {
+
+
+        private readonly IMapper _mapper;
+
+        private readonly IAccountCrudService _accountService;
+
+        public AccountController(IMapper mapper, IAccountCrudService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        // POST api/account
+        [HttpPost]
+        public ActionResult Post([FromBody]AccountViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var account = new Account()
+                {
+                    Email = viewModel.Email,
+                    Password = viewModel.Password.HashPassword(),
+                };
+
+                _accountService.Insert(account);
+            } catch(Exception ex) { }
+
+            return Ok();
+        }
+
+        // POST api/account/authenticate
+        [HttpPost("authenticate")]
+        public ActionResult<string> Authenticate([FromBody] AccountViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var account = _accountService.Authenticate(viewModel.Email, viewModel.Password);
+
+            if (account == null)
+                return Unauthorized();
+
+            return Ok(account.Token);
+        }
+    }
+}
---
+++ src/Knewin.CityApi/ViewModels/Account/AccountViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Knewin.CityApi.ViewModels.Account
+{
+    public class AccountViewModel
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+}
